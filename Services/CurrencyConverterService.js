8a224c6d59fe9f39efbf48ac2faa5eca78f1bb8f
Idanshoham <Idanshoham1@gmail.com>
2021-01-07 21:48:21 +0200

    cleaner code

--- Services/CurrencyConverterService.js
+++ Services/CurrencyConverterService.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 import { CURRENCY_CONVERTER_API_URL } from '../Constants/apiConstants.js';
 
-export const getSpecificRateBetween = (fromCurrency, toCurrency) => {
-    return axios.get(
+export const getCoversionRate = async (fromCurrency, toCurrency) => {
+    const result = await axios.get(
         `${CURRENCY_CONVERTER_API_URL}?base=${fromCurrency}&symbols=${toCurrency}`
-    ).then(result => result.data.rates[toCurrency]);
+    );
+
+    return result.data.rates[toCurrency];
 }
--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 import { getConvertedFromCurrencyValues } from './Services/FileParser.js';
-import { getSpecificRateBetween } from './Services/CurrencyConverterService.js';
+import { getCoversionRate } from './Services/CurrencyConverterService.js';
 
 const getResults = async () => {
     try {
         let myArgs = process.argv.slice(2); // getting the relevant commandline arguments
         console.log('before:');
-        const result = await getConvertedFromCurrencyValues(myArgs[0], getSpecificRateBetween);
+        const result = await getConvertedFromCurrencyValues(myArgs[0], getCoversionRate);
         console.log('after:');
         console.log(result);
     } catch(err) {
