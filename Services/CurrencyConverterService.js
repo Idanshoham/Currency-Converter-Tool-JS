a5cf22342613fa7ea615e47300d9e36e8f957843
Idanshoham <Idanshoham1@gmail.com>
2021-01-07 21:49:52 +0200

    fix func name

--- Services/CurrencyConverterService.js
+++ Services/CurrencyConverterService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { CURRENCY_CONVERTER_API_URL } from '../Constants/apiConstants.js';
 
-export const getCoversionRate = async (fromCurrency, toCurrency) => {
+export const getConversionRate = async (fromCurrency, toCurrency) => {
     const result = await axios.get(
         `${CURRENCY_CONVERTER_API_URL}?base=${fromCurrency}&symbols=${toCurrency}`
     );
--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 import { getConvertedFromCurrencyValues } from './Services/FileParser.js';
-import { getCoversionRate } from './Services/CurrencyConverterService.js';
+import { getConversionRate } from './Services/CurrencyConverterService.js';
 
 const getResults = async () => {
     try {
         let commandlineArgs = process.argv.slice(2); // getting the relevant commandline arguments
         console.log('before:');
-        const result = await getConvertedFromCurrencyValues(commandlineArgs[0], getCoversionRate);
+        const result = await getConvertedFromCurrencyValues(commandlineArgs[0], getConversionRate);
         console.log('after:');
         console.log(result);
     } catch(err) {
